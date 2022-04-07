6ff12bd585dd6dacecb78e8503d697f3f05c68cf
aleeshafarnek <102582643+aleeshafarnek@users.noreply.github.com>
2022-04-07 11:25:53 +0400

    Removed unwanted routes

--- app/Config/RouteApi.php
+++ app/Config/RouteApi.php
@@ -6,22 +6,6 @@
  */
 //-------------------------------------  ALEESHA CODES ------------------------------------ //
 
-// using raw sql
-
-$routes->post('searchname', 'EmployeesController::searchBySQL');
-$routes->post('employee',   'EmployeesController::createBySQL');
-$routes->get('employee',    'EmployeesController::indexBySQL');
-$routes->get('employee/(:segment)',    'EmployeesController::showBySQL/$1');
-$routes->post('employee/(:segment)',    'EmployeesController::updateBySQL/$1');
-$routes->delete('employee/(:segment)',   'EmployeesController::DeleteBySQL/$1');
-
-// // using build in query functions
-// $routes->post('searchname', 'employee::filterByName');
-// $routes->post('employee',                'employee::create');
-// $routes->get('employee',                 'employee::index');
-// $routes->get('employee/(:segment)',      'employee::show/$1');
-// $routes->post('employee/(:segment)',    'employee::update/$1');
-// $routes->delete('employee/(:segment)',   'employee::delete/$1');
 
 //-----------  FLEXI GUEST API ROUTES -----------------//
 
@@ -42,13 +26,6 @@ $routes->group("api", function ($routes) {
 
 
 
-//  print_r($this->router->routes);exit;
-// // using raw sql
-// $routes->post('searchname', 'EmployeesController::searchBySQL');
-// // $routes->post('employee',                'EmployeesController::createBySQL');
-// $routes->get('employee',                 'EmployeesController::indexBySQL');
-// //$routes->get('employee/(:segment)',      'employeesController::showBySQL/$1');
-// //$routes->post('employee/(:segment)',    'employeesController::updateBySQL/$1');
-// //$routes->delete('employee/(:segment)',   'employeesController::DeleteBySQL/$1');
+
 
 //  ------------------------------------ALEESHA CODES --------------------------------------- //
