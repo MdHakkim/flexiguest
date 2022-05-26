94bcfcbc167520fb323e5e3bb90840b841d52dda
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-05-26 08:31:07 +0400

    updated AuthAPI.php

--- app/Filters/AuthAPI.php
+++ app/Filters/AuthAPI.php
@@ -1,54 +1,43 @@
 <?php
+
 namespace App\Filters;
 
 use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
-use CodeIgniter\Session\Session;
 
 helper('responsejson');
 
 class AuthAPI implements FilterInterface
-{           
+{
     // checking JWT Token valid or not
     public function before(RequestInterface $request, $arguments = null)
     {
-            try {
+        try {
 
-                helper('jwt');
-                
-                $token = explode(" ", getJWTFromRequest())[1];
+            helper('jwt');
 
-                $decoded  = validateJWTFromRequest($token);
-               
-                if(!empty($decoded)){
+            $token = explode(" ", getJWTFromRequest())[1];
 
-                    if(isset($arguments[0]) && isset($decoded['token_info']->data->USR_ROLE) && $arguments[0] == $decoded['token_info']->data->USR_ROLE){
-                    
-                        $request->user = $decoded['table_info'];
-                        return $request;
-                    }
+            $decoded  = validateJWTFromRequest($token);
+
+            if (!empty($decoded)) {
+
+                if (isset($arguments[0]) && isset($decoded['token_info']->data->USR_ROLE) && $arguments[0] == $decoded['token_info']->data->USR_ROLE) {
+
+                    $request->user = $decoded['table_info'];
+                    return $request;
                 }
-
-                $result = [
-                    "status" =>401,
-                    "error" => true,
-                    "message"=>'User Unauthorized',
-                    "data"=> []
-                ];
-
-                echo json_encode($result);die;                 
-            }catch (Exception $ex) {
-                 
-                
-                $result = ["status" =>401,
-                    "error" => true,
-                    "message"=>'User Unauthorized',
-                    "data"=> []];
-                echo json_encode($result);die;
             }
 
-            
+            $result = responseJson(401, true, "User Unauthorized", []);
+            echo json_encode($result);
+            die;
+        } catch (\Exception $ex) {
+            $result = responseJson(401, true, "User Unauthorized", []);
+            echo json_encode($result);
+            die;
+        }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
