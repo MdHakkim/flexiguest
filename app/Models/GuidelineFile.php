dfdbc685aa87ae2c5410e318dbac440818acd02b
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-06-06 13:08:23 +0400

    guidleline => DB naming convention updated

--- app/Models/GuidelineFile.php
+++ app/Models/GuidelineFile.php
@@ -6,20 +6,20 @@ use CodeIgniter\Model;
 
 class GuidelineFile extends Model
 {
-    protected $table      = 'guideline_files';
-    protected $primaryKey = 'id';
+    protected $table      = 'FLXY_GUIDELINE_FILES';
+    protected $primaryKey = 'GLF_ID';
     protected $allowedFields = [
-        'guideline_id',
-        'file_url',
-        'file_type',
-        'file_name',
-        'created_by',
-        'updated_by',
+        'GLF_GUIDELINE_ID',
+        'GLF_FILE_URL',
+        'GLF_FILE_TYPE',
+        'GLF_FILE_NAME',
+        'GLF_CREATED_BY',
+        'GLF_UPDATED_BY',
     ];
 
     protected $useAutoIncrement = true;
 
     protected $useTimestamps = true;
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
+    protected $createdField  = 'GLF_CREATED_AT';
+    protected $updatedField  = 'GLF_UPDATED_AT';
 }
