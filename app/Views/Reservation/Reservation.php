775f6fd212c77d3a965fff2a05e1d20d6bad4825
subinakk <100277089+subinakk@users.noreply.github.com>
2022-07-21 08:10:28 +0400

    Reservation changes

--- app/Views/Reservation/Reservation.php
+++ app/Views/Reservation/Reservation.php
@@ -2382,7 +2382,7 @@
 
     $(document).on('click', '.editReserWindow,#triggCopyReserv', function(event, param, paramArr, rmtype) {
         ////Item Inventory
-        showInventoryItems();
+       // showInventoryItems();
         itemClassList();
 
         $(':input', '#reservationForm').val('').prop('checked', false).prop('selected', false);
