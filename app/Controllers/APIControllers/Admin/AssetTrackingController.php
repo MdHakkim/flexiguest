91b23a815b56551e668de6dce548f2ed60a95fd3
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-08-15 11:10:52 +0400

    asset tracking working...

--- app/Controllers/APIControllers/Admin/AssetTrackingController.php
+++ app/Controllers/APIControllers/Admin/AssetTrackingController.php
@@ -38,12 +38,23 @@ class AssetTrackingController extends BaseController
     {
         $rooms = $this->Reservation
             ->select("distinct(RESV_ID), RESV_ROOM as ROOM_NO, RRA_ROOM_ID as ROOM_ID, 
-                    concat(CUST_FIRST_NAME, ' ', CUST_LAST_NAME) as GUEST_NAME")
+                    concat(CUST_FIRST_NAME, ' ', CUST_LAST_NAME) as GUEST_NAME,
+                    RRA_STATUS")
             ->join('FLXY_RESERVATION_ROOM_ASSETS', 'RESV_ID = RRA_RESERVATION_ID', 'left')
             ->join('FLXY_CUSTOMER', 'RESV_NAME = CUST_ID', 'left')
             ->where('RESV_STATUS', 'Checked-In')
             ->where('RRA_STATUS', 'Completed')
             ->findAll();
+
+        foreach ($rooms as $index => $room) {
+            $rooms[$index]['ASSETS'] = $this->ReservationRoomAsset
+                ->select('AS_ID, AS_ASSET, RA_QUANTITY, RRA_STATUS, RRA_TRACKING_REMARKS')
+                ->join('FLXY_ROOM_ASSETS', 'RRA_ROOM_ID = RA_ROOM_ID', 'left')
+                ->join('FLXY_ASSETS', 'RRA_ASSET_ID = AS_ID', 'left')
+                ->where('RRA_RESERVATION_ID', $room['RESV_ID'])
+                ->where('RRA_ROOM_ID', $room['ROOM_ID'])
+                ->findAll();
+        }
 
         return $this->respond(responseJson(200, false, ['msg' => 'assets list'], $rooms));
     }
