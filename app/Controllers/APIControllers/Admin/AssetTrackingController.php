39db74315d44e35bda37e8474a19eb0b72617071
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-08-16 11:09:03 +0400

    AssetTrackingController.php updated

--- app/Controllers/APIControllers/Admin/AssetTrackingController.php
+++ app/Controllers/APIControllers/Admin/AssetTrackingController.php
@@ -48,9 +48,9 @@ class AssetTrackingController extends BaseController
 
         foreach ($rooms as $index => $room) {
             $rooms[$index]['ASSETS'] = $this->ReservationRoomAsset
-                ->select("AS_ID, AS_ASSET, RA_QUANTITY, RRA_STATUS, RRA_TRACKING_REMARKS,
+                ->select("AS_ID, AS_ASSET, RA_QUANTITY, RRA_STATUS, RRA_TRACKING_REMARKS, RRA_ID,
                 case when RRA_STATUS = 'Verified' then 1 else 0 end as IS_VERIFIED")
-                ->join('FLXY_ROOM_ASSETS', 'RRA_ROOM_ID = RA_ROOM_ID', 'left')
+                ->join('FLXY_ROOM_ASSETS', 'RRA_ROOM_ID = RA_ROOM_ID and  RRA_ASSET_ID = RA_ASSET_ID', 'left')
                 ->join('FLXY_ASSETS', 'RRA_ASSET_ID = AS_ID', 'left')
                 ->where('RRA_RESERVATION_ID', $room['RESV_ID'])
                 ->where('RRA_ROOM_ID', $room['ROOM_ID'])
