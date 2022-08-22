f78beabcdbd7f351277e770b2e5caa678bece6e0
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-08-22 16:10:35 +0400

    GuidelineRepository updated

--- app/Controllers/APIControllers/Guest/GuidelineController.php
+++ app/Controllers/APIControllers/Guest/GuidelineController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\APIControllers\Guest;
 
 use App\Controllers\BaseController;
+use App\Controllers\Repositories\GuidelineRepository;
 use App\Models\Guideline;
 use App\Models\GuidelineFile;
 use CodeIgniter\API\ResponseTrait;
@@ -11,31 +12,20 @@ class GuidelineController extends BaseController
 {
     use ResponseTrait;
 
+    private $GuidelineRepository;
     private $Guideline;
     private $GuidelineFile;
 
     public function __construct()
     {
+        $this->GuidelineRepository = new GuidelineRepository();
         $this->Guideline = new Guideline();
         $this->GuidelineFile = new GuidelineFile();
     }
 
     public function guideline()
     {
-        $guidelines = $this->Guideline->orderBy('GL_ID', 'desc')->findAll();
-        
-        foreach($guidelines as $index => $item){
-            $guidelines[$index]['GL_COVER_IMAGE'] = base_url($item['GL_COVER_IMAGE']);
-
-            $guideline_files = $this->GuidelineFile->where('GLF_GUIDELINE_ID', $item['GL_ID'])->findAll();
-            foreach($guideline_files as $j => $file){
-                $guideline_files[$j]['GLF_FILE_URL'] = base_url($file['GLF_FILE_URL']);
-            }
-
-            $guidelines[$index]['GLF_GUIDELINE_FILES'] = $guideline_files;
-        }
-
-        $result = responseJson(200, false, 'Guidelines list', $guidelines);
-        return $this->respond($result);
+        $response = $this->GuidelineRepository->allGuidelines();
+        return $this->respond($response);
     }
 }
--- app/Controllers/Repositories/GuidelineRepository.php
+++ app/Controllers/Repositories/GuidelineRepository.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Repositories;
 
 use App\Controllers\BaseController;
 use App\Models\Guideline;
+use App\Models\GuidelineFile;
 use CodeIgniter\API\ResponseTrait;
 
 class GuidelineRepository extends BaseController
@@ -11,10 +12,12 @@ class GuidelineRepository extends BaseController
     use ResponseTrait;
 
     private $Guideline;
+    private $GuidelineFile;
 
     public function __construct()
     {
         $this->Guideline = new Guideline();
+        $this->GuidelineFile = new GuidelineFile();
     }
 
     public function disableEnableGuideline($guideline_id)
@@ -28,5 +31,23 @@ class GuidelineRepository extends BaseController
         $this->Guideline->save($guideline);
 
         return responseJson(200, false, ['msg' => $msg]);
+    }
+
+    public function allGuidelines()
+    {
+        $guidelines = $this->Guideline->where('GL_IS_ENABLED', 1)->orderBy('GL_ID', 'desc')->findAll();
+        
+        foreach($guidelines as $index => $item) {
+            $guidelines[$index]['GL_COVER_IMAGE'] = base_url($item['GL_COVER_IMAGE']);
+
+            $guideline_files = $this->GuidelineFile->where('GLF_GUIDELINE_ID', $item['GL_ID'])->findAll();
+            foreach($guideline_files as $j => $file){
+                $guideline_files[$j]['GLF_FILE_URL'] = base_url($file['GLF_FILE_URL']);
+            }
+
+            $guidelines[$index]['GLF_GUIDELINE_FILES'] = $guideline_files;
+        }
+
+        return responseJson(200, false, 'Guidelines list', $guidelines);
     }
 }
