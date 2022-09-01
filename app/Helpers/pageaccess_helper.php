eb57ad8b7739d7b3b93a3294a59aa4d4ed2f9039
subinakk <100277089+subinakk@users.noreply.github.com>
2022-09-01 10:48:01 +0400

    Page Access Fixes

--- app/Helpers/pageaccess_helper.php
+++ app/Helpers/pageaccess_helper.php
@@ -20,10 +20,12 @@ function checkPageAccess()
     //$url = explode("/",$url);
     $url = explode("/", parse_url($url, PHP_URL_PATH));
    
-    $ROLE_ID = session()->get('USR_ROLE_ID');    
-
+    $ROLE_ID = session()->get('USR_ROLE_ID');  
     if(isset($url[1]) && $url[1] != ''){
-         $sql = "SELECT * FROM FLXY_USER_ROLE_PERMISSION INNER JOIN FLXY_MENU ON MENU_ID = ROLE_MENU_ID WHERE ROLE_ID = '$ROLE_ID' AND ROLE_PERM_STATUS = 1 AND MENU_URL LIKE '%$url[0]/$url[1]%'";
+        if(is_numeric($url[1]))
+            $sql = "SELECT * FROM FLXY_USER_ROLE_PERMISSION INNER JOIN FLXY_MENU ON MENU_ID = ROLE_MENU_ID WHERE ROLE_ID = '$ROLE_ID' AND ROLE_PERM_STATUS = 1 AND MENU_URL LIKE '%$url[0]%'";
+        else 
+            $sql = "SELECT * FROM FLXY_USER_ROLE_PERMISSION INNER JOIN FLXY_MENU ON MENU_ID = ROLE_MENU_ID WHERE ROLE_ID = '$ROLE_ID' AND ROLE_PERM_STATUS = 1 AND MENU_URL LIKE '%$url[0]/$url[1]%'";
     }
     else{
         $sql = "SELECT * FROM FLXY_USER_ROLE_PERMISSION INNER JOIN FLXY_MENU ON MENU_ID = ROLE_MENU_ID WHERE ROLE_ID = '$ROLE_ID' AND ROLE_PERM_STATUS = 1 AND MENU_URL LIKE '$url[0]'";
