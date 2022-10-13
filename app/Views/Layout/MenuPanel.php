ba25e11501280f5e688500950059f6ed233066b8
subinakk <100277089+subinakk@users.noreply.github.com>
2022-10-13 10:01:15 +0400

    Notification Heading change

--- app/Views/Layout/MenuPanel.php
+++ app/Views/Layout/MenuPanel.php
@@ -262,7 +262,7 @@
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="popModalWindowlabel">All Notifications</h4>
+                    <h4 class="modal-title" id="popModalWindowlabel1">All Notifications</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
