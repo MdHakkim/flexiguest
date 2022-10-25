95e64495a1c96bee3f8332b84f37ca6610a58d0f
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-10-25 12:42:13 +0400

    minor change

--- app/Controllers/Repositories/NotificationRepository.php
+++ app/Controllers/Repositories/NotificationRepository.php
@@ -29,10 +29,11 @@ class NotificationRepository extends BaseController
 				"priority" => "HIGH",
 				"registration_ids" => ["fWrB40jNSG-hmSFLR0PQMx:APA91bFZBax526Ww53rQviLSvwDZykMxbUJ05rNi8nbFJtlwajHh8wsEXwbeqseI2WRCFzmpPhX6pOLHNvSB5668K_cxSc9I9cxl2ZsMb7N_s_DNg2Hz4mvool1WesTLR6xrrB6vbBX5"],
 				"data" => [
-					"title" => "my-data-item",
-					"body" => "Portal",
 					"badge" => "1",
 					"content_available" => true,
+					
+					"title" => "my-data-item",
+					"body" => "Portal",
 					"screen" => "heelo"
 				]
 			]
