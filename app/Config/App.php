977426137c414291be8c21044c3e3084b6b12188
deleepbose-farnek <deleep.bose@farnek.com>
2022-11-05 17:33:58 +0400

    app | database.php updated

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/FlexiGuest/';
+    public $baseURL = 'https://flexiguest.hitekservices.com/';
 
     /**
      * --------------------------------------------------------------------------
