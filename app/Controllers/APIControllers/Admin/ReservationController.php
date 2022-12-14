325365c7b814e7950e13c8ec264e5e69e97ee154
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-12-14 09:29:36 +0400

    getReservationsList()

--- app/Controllers/APIControllers/Admin/ReservationController.php
+++ app/Controllers/APIControllers/Admin/ReservationController.php
@@ -23,14 +23,15 @@ class ReservationController extends BaseController
     public function getReservationsList()
     {
         $reservsations = $this->Reservation
-                                ->select('RESV_ID, RESV_ARRIVAL_DT, RESV_DEPARTURE, RESV_NIGHT, RESV_ADULTS, 
+                                ->select("RESV_ID, RESV_ARRIVAL_DT, RESV_DEPARTURE, RESV_NIGHT, RESV_ADULTS, 
                                         RESV_CHILDREN, RESV_STATUS, RESV_ROOM, 
                                         fc.CUST_ID, fc.CUST_FIRST_NAME, fc.CUST_MIDDLE_NAME, fc.CUST_LAST_NAME, fc.CUST_TITLE, fc.CUST_DOB, 
+                                        concat(fc.CUST_FIRST_NAME, ' ', fc.CUST_LAST_NAME) as NAME,
                                         fc.CUST_ADDRESS_1, fc.CUST_ADDRESS_2, fc.CUST_ADDRESS_3, fc.CUST_COUNTRY, fc.CUST_CITY,
                                         fc.CUST_EMAIL, fc.CUST_MOBILE_CODE, fc.CUST_MOBILE, fc.CUST_NATIONALITY, fc.CUST_COR, fc.CUST_DOC_NUMBER, fc.CUST_DOC_TYPE, 
                                         fc.CUST_DOC_ISSUE, fc.CUST_DOC_EXPIRY, fc.CUST_GENDER, 
                                         fr.RM_DESC, 
-                                        CITY.ctname') 
+                                        CITY.ctname") 
                                         // fvd.VACC_DETAILS, fvd.VACC_IS_VERIFY, fvd.VACC_ISSUED_COUNTRY')
                                 ->join('FLXY_CUSTOMER as fc', 'FLXY_RESERVATION.RESV_NAME = fc.CUST_ID', 'left')
                                 ->join('FLXY_ROOM as fr', 'FLXY_RESERVATION.RESV_ROOM = fr.RM_NO', 'left')
