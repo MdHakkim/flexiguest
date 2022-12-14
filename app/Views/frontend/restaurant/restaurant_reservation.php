39b7e0cc3f449be29240012f2b00a484b8043e6d
abu-bakar-safder <102672190+abu-bakar-safder@users.noreply.github.com>
2022-12-14 11:04:14 +0400

    restaurant_reservation.php updated

--- app/Views/frontend/restaurant/restaurant_reservation.php
+++ app/Views/frontend/restaurant/restaurant_reservation.php
@@ -26,7 +26,7 @@
                             <th>Slot</th>
                             <th>No of Guests</th>
                             <th>Created At</th>
-                            <th class="all">Action</th>
+                            <!-- <th class="all">Action</th> -->
                         </tr>
                     </thead>
                 </table>
@@ -36,55 +36,6 @@
         <!--/ Multilingual -->
     </div>
     <!-- / Content -->
-
-    <!-- Modal Window -->
-
-    <div class="modal fade" id="popModalWindow" data-backdrop="static" data-keyboard="false" aria-lableledby="popModalWindowlable">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <h4 class="modal-title" id="popModalWindowlabel">Restuarant</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-lable="Close"></button>
-                </div>
-
-                <div class="modal-body">
-                    <form id="submit-form" class="needs-validation" novalidate>
-                        <div class="row g-3">
-                            <input type="hidden" name="id" class="form-control" />
-
-                            <div class="col-md-6">
-                                <label class="form-label"><b>Restaurant Name *</b></label>
-                                <input type="text" name="RE_RESTAURANT" class="form-control" placeholder="Restaurant Name" required />
-                            </div>
-
-                            <div class="col-md-6">
-                                <label class="form-label"><b>Seating Capacity *</b></label>
-                                <input type="number" name="RE_SEATING_CAPACITY" class="form-control" required />
-                            </div>
-
-                            <div class="col-md-12">
-                                <label class="form-label" for="RE_IMAGE_URL"><b>Restaurant Image *</b></label>
-                                <input type="file" name="RE_IMAGE_URL" id="RE_IMAGE_URL" class="form-control" />
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-
-                    <button type="button" id="submitBtn" onClick="submitForm()" class="btn btn-primary">
-                        Save
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- /Modal window -->
 
     <div class="content-backdrop fade"></div>
 </div>
@@ -140,37 +91,37 @@
                 {
                     data: 'RR_CREATED_AT'
                 },
-                {
-                    data: null,
-                    className: "text-center",
-                    "orderable": false,
-                    render: function(data, type, row, meta) {
-                        return (
-                            `
-                        <div class="d-inline-block">
-                            <a href="javascript:;" title="Edit or Delete" class="btn btn-sm btn-primary btn-icon rounded-pill dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                                <i class="bx bx-dots-vertical-rounded"></i>
-                            </a>
+                // {
+                //     data: null,
+                //     className: "text-center",
+                //     "orderable": false,
+                //     render: function(data, type, row, meta) {
+                //         return (
+                //             `
+                //         <div class="d-inline-block">
+                //             <a href="javascript:;" title="Edit or Delete" class="btn btn-sm btn-primary btn-icon rounded-pill dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                //                 <i class="bx bx-dots-vertical-rounded"></i>
+                //             </a>
 
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li>
-                                    <a href="javascript:;" data_id="${data['RE_ID']}" class="dropdown-item editWindow text-primary">
-                                        <i class="fa-solid fa-pen-to-square"></i> Edit
-                                    </a>
-                                </li>
+                //             <ul class="dropdown-menu dropdown-menu-end">
+                //                 <li>
+                //                     <a href="javascript:;" data_id="${data['RE_ID']}" class="dropdown-item editWindow text-primary">
+                //                         <i class="fa-solid fa-pen-to-square"></i> Edit
+                //                     </a>
+                //                 </li>
 
-                                <div class="dropdown-divider"></div>
+                //                 <div class="dropdown-divider"></div>
                                 
-                                <li>
-                                    <a href="javascript:;" data_id="${data['RE_ID']}" class="dropdown-item text-danger delete-record">
-                                        <i class="fa-solid fa-ban"></i> Delete
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    `);
-                    }
-                },
+                //                 <li>
+                //                     <a href="javascript:;" data_id="${data['RE_ID']}" class="dropdown-item text-danger delete-record">
+                //                         <i class="fa-solid fa-ban"></i> Delete
+                //                     </a>
+                //                 </li>
+                //             </ul>
+                //         </div>
+                //     `);
+                //     }
+                // },
             ],
             columnDefs: [{
                 width: "7%",
@@ -239,145 +190,6 @@
         // $("#dataTable_view_wrapper .row:first").before(
         //     '<div class="row flxi_pad_view"><div class="col-md-3 ps-0"><button type="button" class="btn btn-primary" onClick="addForm()"><i class="fa-solid fa-plus fa-lg"></i> Add</button></div></div>'
         // );
-    });
-
-    function resetForm() {
-        $(`${form_id} input`).val('');
-        $(`${form_id} textarea`).val('');
-        $(`${form_id} select`).val('').trigger('change');
-
-        $(`${form_id} input[name='RE_SEATING_CAPACITY']`).val('1');
-    }
-
-    // Show Add Rate Class Form
-    function addForm() {
-        resetForm();
-
-        $('#submitBtn').removeClass('btn-success').addClass('btn-primary').text('Save');
-        $('#popModalWindowlabel').html('Add Restaurant');
-
-        $('#popModalWindow').modal('show');
-    }
-
-    // Add New or Edit Rate Class submit Function
-    function submitForm() {
-        hideModalAlerts();
-        var fd = new FormData($(`${form_id}`)[0]);
-        fd.delete('RE_IMAGE_URL');
-
-        let files = $(`${form_id} input[name='RE_IMAGE_URL']`)[0].files;
-        if (files.length)
-            fd.append('RE_IMAGE_URL', files[0]);
-
-        $.ajax({
-            url: '<?= base_url('/restaurant/store-restaurant') ?>',
-            type: "post",
-            data: fd,
-            processData: false,
-            contentType: false,
-            dataType: 'json',
-            success: function(response) {
-                if (response['SUCCESS'] != 200) {
-
-                    var ERROR = response['RESPONSE']['REPORT_RES'];
-                    var mcontent = '';
-                    $.each(ERROR, function(ind, data) {
-
-                        mcontent += '<li>' + data + '</li>';
-                    });
-                    showModalAlert('error', mcontent);
-                } else {
-                    var alertText = response['RESPONSE']['REPORT_RES']['msg'];
-
-                    showModalAlert('success', alertText);
-
-                    $('#popModalWindow').modal('hide');
-                    $('#dataTable_view').dataTable().fnDraw();
-                }
-            }
-        });
-    }
-
-    // Show Edit Rate Class Form
-    $(document).on('click', '.editWindow', function() {
-        resetForm();
-
-        $('.dtr-bs-modal').modal('hide');
-        var restaurant_id = $(this).attr('data_id');
-
-        $(`${form_id} input[name='id']`).val(restaurant_id);
-
-        $('#popModalWindowlabel').html('Edit Restaurant');
-        $('#popModalWindow').modal('show');
-
-        var url = '<?php echo base_url('/restaurant/edit-restaurant') ?>';
-        $.ajax({
-            url: url,
-            type: "post",
-            data: {
-                id: restaurant_id
-            },
-            dataType: 'json',
-            success: function(response) {
-                $(response).each(function(inx, data) {
-                    $.each(data, function(field, val) {
-                        if ($(`${form_id} input[name='${field}'][type!='file']`).length)
-                            $(`${form_id} input[name='${field}']`).val(val);
-
-                        else if ($(`${form_id} textarea[name='${field}']`).length)
-                            $(`${form_id} textarea[name='${field}']`).val(val);
-                    });
-                });
-
-                $('#submitBtn').removeClass('btn-primary').addClass('btn-success').text('Update');
-            }
-        });
-    });
-
-    // Delete Rate Class
-    $(document).on('click', '.delete-record', function() {
-        hideModalAlerts();
-        $('.dtr-bs-modal').modal('hide');
-
-        var id = $(this).attr('data_id');
-        bootbox.confirm({
-            message: "Are you sure you want to delete this record?",
-            buttons: {
-                confirm: {
-                    label: 'Yes',
-                    className: 'btn-success'
-                },
-                cancel: {
-                    label: 'No',
-                    className: 'btn-danger'
-                }
-            },
-            callback: function(result) {
-                if (result) {
-                    $.ajax({
-                        url: '<?php echo base_url('/restaurant/delete-restaurant') ?>',
-                        type: "post",
-                        data: {
-                            id: id,
-                            '_method': 'delete'
-                        },
-                        headers: {
-                            'X-Requested-With': 'XMLHttpRequest'
-                        },
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response['SUCCESS'] != 200) {
-                                showModalAlert('error', response['RESPONSE']['REPORT_RES']['msg']);
-                            } else {
-                                showModalAlert('success', response['RESPONSE']['REPORT_RES']['msg']);
-
-                                $('#dataTable_view').dataTable().fnDraw();
-                            }
-                        }
-                    });
-                }
-            }
-        });
     });
 
     // bootstrap-maxlength & repeater (jquery)
